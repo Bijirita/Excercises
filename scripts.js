27d5e9fd786beac4106aae94023eb6e614bb04ab
Bijirita <bijirita009@gmail.com>
2018-10-05 15:41:47 -0400

    exerceise

---
+++ scripts.js
@@ -0,0 +1,33 @@
+// let cohort = {
+//     name: "Nestor",
+//     age: 28,
+//     sex: "Male",
+// }
+
+
+let words = ["apple", "bat","calendar","dispel","effort","flag","gnosis","helicopter","ingenious","jurassic","ketones"]
+
+// // console.log(Object.values(cohort));
+
+let TextBox = document.getElementById("TextBox");
+const PressMe = document.getElementById("Press");
+PressMe.addEventListener("click", generate);
+
+function generate() {
+    let myArray = words.reduce((accumulator) => {
+        let index = Math.floor(Math.random() * words.length-1);
+        accumulator.push(words[index]);
+        return accumulator;
+    },[]);
+    console.log(myArray.join(' '));
+}
+
+
+
+const filterThis = ["array", "should", "have", "at", "least", "five", "items"];
+
+
+
+
+
+
